396f2b8cf78546ea51edda979f4ede1b2461c5c5
djaydevs <80023967+djaydevs@users.noreply.github.com>
2022-12-15 22:23:11 +0800

    update added top navigation bar

---
+++ docfillup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="docfillup.css">
+    <title>Document Fill-Up Automation Website</title>
+</head>
+<body>
+    <div class="topnav">
+        <img class="logo" src="assets/fatimalogo.svg" alt="logo">
+        <p class="tntitle">Barangay Fatima 1</p>
+        <nav>
+            <ul>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Residents Data</a></li>
+                <li><a class="active" href="#">Document Fill-Up</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
