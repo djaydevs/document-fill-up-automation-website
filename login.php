f1e30f6d6f6d974bbf42b60f97b71ab1085734c7
codeHobi <119099153+codeHobi@users.noreply.github.com>
2022-12-18 03:22:28 +0800

    log in - temp

--- login.php
+++ login.php
@@ -10,25 +10,31 @@ include("connection.php");
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="login.css">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="login.css ?v=<?php echo time(); ?>">
     <title>Document Fill-Up Automation Website - Log in</title>
 </head>
 <body>
-    <div class="top-nav">
-        <img class="logo" src="assets/fatimalogo.svg" alt="logo">      
-        <form action="authentication.php" method="POST">
-            <h2>LOGIN</h2>        
-            <?php if (isset($_GET['error'])) { ?>
-                <p class="error"><?php echo $_GET['error']; ?></p>
-            <?php } ?>        
-            <label>Username:</label>
-            <input type="text" name="uname" placeholder="Username"><br>
+    <div class="grid-container">
+        <div class="leftsection">
+            <img src="assets/logovector.svg" alt="">
+            <p class="title">Document Fill-Up Automation</p>
+            <p class="description">Document request and fill-up is now easy and convenient!</p>
+        </div>
+        <div class="rightsection">      
+            <form action="authentication.php" method="POST">
+                <h2>LOGIN</h2>        
+                <?php if (isset($_GET['error'])) { ?>
+                    <p class="error"><?php echo $_GET['error']; ?></p>
+                <?php } ?>        
+                <label>Username:</label><br>
+                <input type="text" name="uname" class="input-user" placeholder="Username"><br>
 
-            <label>Password:</label>
-            <input type="password" name="password" placeholder="Password"><br>
+                <label>Password:</label><br>
+                <input type="password" name="password" class="input-pass" placeholder="Password"><br>
 
-            <button type="submit">Login</button>
-        </form>
+                <input type="submit" class="btn-login" value="LOGIN">
+        </div>
     </div>
 </body>
 </html>
