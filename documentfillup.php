d9d384fc4e3b89fefaed480bfde26cde0c2be6fd
djaydevs <80023967+djaydevs@users.noreply.github.com>
2022-12-20 20:24:35 +0800

    commit

--- documentfillup.php
+++ documentfillup.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/documentfillup.css ?v=<?php echo time(); ?>">
-        <title>Document Fill-Up Automation - Certificate of Indigency</title>
+        <title>Document Fill-Up Automation</title>
     </head>
     <body>
         <!-- NAVIGATION BAR -->
